bc004fda9ad174918527f6c3cbd17bc35534d816
Abhinandan Adhikari <88623966+AbhinandanAdhikari@users.noreply.github.com>
2023-02-28 13:54:13 +0530

    Added Week 2 Tasks

---
+++ Week-2/Week2/src/week2Practice/Task5.java
@@ -0,0 +1,42 @@
+// Task 5: Write a program to convert currency in respective country using switch case.
+package week2Practice;
+import java.util.*;
+
+public class Task5 {
+	
+	static void convertCurrency(double rupee, String country)
+	{
+		double amount = 0;
+		switch(country)
+		{
+		case "US":
+			amount = rupee * 0.0121;
+			System.out.println("$"+amount);
+			break;
+		case "AUS":
+			amount = rupee * 0.0179;
+			System.out.println(amount);
+			break;
+		case "UAE":
+			amount = rupee * 0.0443;
+			System.out.println(amount);
+			break;
+		case "CAD":
+			amount = rupee * 0.0164;
+			System.out.println(amount);
+			break;
+		default : 
+			System.out.println("Wrong input");
+			break;
+		}
+	}
+	public static void main(String args[])
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter currency in Indian Rupees");
+		double rupee = in.nextDouble();in.nextLine();
+		System.out.println("Enter the country name: US, AUS, UAE, CAD");
+		String country = in.nextLine();
+		convertCurrency(rupee,country);
+	}
+}
---
+++ Week-2/Week2/src/week2Practice/Task6.java
@@ -0,0 +1,28 @@
+//Task 6: Write a program to find highest of three numbers.
+package week2Practice;
+import java.util.*;
+public class Task6 {
+	
+	static int highest(int num1, int num2, int num3)
+	{
+		if(num1 > num2 && num1 > num3)
+		{
+			return num1;
+		}
+		else if(num2 > num1 && num2 > num3)
+		{
+			return num2;
+		}
+		return num3;
+	}
+	
+	public static void main(String args[])
+	{
+		Scanner in = new Scanner(System.in);
+		int num1 = in.nextInt();
+		int num2 = in.nextInt();
+		int num3 = in.nextInt();
+		int max = highest(num1,num2,num3);
+		System.out.println(max);
+	}
+}

4cf75c3ed96163d2e8e2b6d8b75ebfd7d931271f
Abhinandan Adhikari <abhinandan.adhikari2002@gmail.com>
2023-03-02 20:22:19 +0530

    Week 2 Assignments by Nakul Sir

---
+++ Week 2/src/week2AssignmentsByNakulSir/Task6.java
@@ -0,0 +1,18 @@
+/* Task 6: Write a Java Program to prit the sum,multiplication,subtraction,division,remainder of two numbers. */
+
+package week2AssignmentsByNakulSir;
+import java.util.*;
+
+public class Task6 {
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        int num1 = in.nextInt();
+        int num2 = in.nextInt();
+        System.out.println(num1+" + "+num2+" = "+(num1+num2));
+        System.out.println(num1+" * "+num2+" = "+(num1*num2));
+        System.out.println(num1+" - "+num2+" = "+Math.abs(num1-num2));
+        System.out.println(num1+" / "+num2+" = "+(num1/num2));
+        System.out.println(num1+" % "+num2+" = "+(num1%num2));
+    }
+}

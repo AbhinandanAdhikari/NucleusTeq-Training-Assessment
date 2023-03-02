3ab72f4ecf2eba560e902ad5af916adb6b877d76
Abhinandan Adhikari <88623966+AbhinandanAdhikari@users.noreply.github.com>
2023-03-02 19:12:34 +0530

    Delete Week-1 directory

--- Week-1/Task2.java
+++
@@ -1,9 +0,0 @@
-/* Task 2: Write a basic "Hello, World!" JAVA Program.*/
-
-import java.lang.*;
-public class Task2{
-    public static void main(String args[])
-    {
-        System.out.println("Hello, World!");
-    }
-}
--- Week-1/Task4.java
+++
@@ -1,8 +0,0 @@
-/* Write a Java Program to print 'Hello' on screen and then print your name on a seperate line */
-
-public class Task4{
-    public static void main(String arg[])
-    {
-        System.out.println("Hello,"+"\n"+"Abhinandan Adhikari");
-    }
-}
